27268facdfca6da957ad942ad0b0f9009535e530
Eugênio Veras <eugeniovma@gmail.com>
2024-03-17 21:47:02 -0300

    root funcionando com a home

---
+++ src/eugenios-org-root-config.ts
@@ -0,0 +1,27 @@
+import { LifeCycles, registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+
+registerApplication({
+  name: "systudo-home-frontend",
+  app: async () => (await System.import("//localhost:4200/main.js")).default,
+  activeWhen: (location) => location.pathname.startsWith("/home"),
+});
+
+start();
