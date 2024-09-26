9320c5f6c7ee0e2cadbc43ad1109fd292578bb0e
Eugênio Veras <eugeniovma@gmail.com>
2024-09-25 23:56:47 -0300

    add login

--- src/eugenios-org-root-config.ts
+++ src/eugenios-org-root-config.ts
@@ -5,6 +5,7 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
+import * as singleSpa from "single-spa"
 
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
@@ -18,6 +19,24 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 layoutEngine.activate();
 
+window.addEventListener('single-spa:before-routing-event', evt => {
+  // redireciona para login quando nao logado
+  if(!window.localStorage.getItem("systudo-token") && window.location.pathname != '/login') {
+    singleSpa.navigateToUrl('/login')
+  }
+
+  // redireciona para pagina nao encontrada
+  const appNames = singleSpa.getAppNames();
+  let app = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+  let index = 0
+  appNames.forEach(name => {
+    if (!(name.includes(app)))
+      index++
+  })
+  if(index == appNames.length)
+    singleSpa.navigateToUrl('/pagenotfound')
+});
+
 registerApplication({
   name: "systudo-home-frontend",
   app: async () => (await System.import("//localhost:4200/main.js")).default,
@@ -25,9 +44,9 @@ registerApplication({
 });
 
 registerApplication({
-  name: "systudo-sidenav-frontend",
+  name: "systudo-login-frontend",
   app: async () => (await System.import("//localhost:4201/main.js")).default,
-  activeWhen: (location) => location.pathname.startsWith("/sidenav"),
+  activeWhen: (location) => location.pathname.startsWith("/login"),
 });
 
 start();
