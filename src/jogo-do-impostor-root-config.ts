d385c453abb696ea64ac6b5a0d54e555a5725e6e
Eugênio Veras <eugeniovma@gmail.com>
2024-10-31 18:39:37 -0300

    root funcionando localhost com game-screen e config-screen

--- src/jogo-do-impostor-root-config.ts
+++ src/jogo-do-impostor-root-config.ts
@@ -19,24 +19,4 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 layoutEngine.activate();
 
-window.addEventListener("single-spa:before-routing-event", (evt) => {
-  if (
-    !window.localStorage.getItem("systudo-token") &&
-    window.location.pathname != "/login"
-  ) {
-    singleSpa.navigateToUrl("/login");
-  }
-
-  // redireciona para pagina nao encontrada
-  const appNames = singleSpa.getAppNames();
-  let app = window.location.href.substring(
-    window.location.href.lastIndexOf("/") + 1
-  );
-  let index = 0;
-  appNames.forEach((name) => {
-    if (!name.includes(app)) index++;
-  });
-  if (index == appNames.length) singleSpa.navigateToUrl("/pagenotfound");
-});
-
 start();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "systudo-org";
+  const orgName = "jogo-do-impostor";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
