f41ee69f81662c3c9b731582194808d425b673c0
Eugênio Veras <eugeniovma@gmail.com>
2024-11-05 18:16:05 -0300

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -16,15 +16,16 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src'),
+        "@": path.resolve(__dirname, "src"),
       },
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     },
     entry: "./src/jogo-do-impostor-root-config",
     devServer: {
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Content-Security-Policy": "default-src 'self' https: localhost:* 192.168.15.8:*; " +
+        "Content-Security-Policy":
+          "default-src 'self' https: localhost:* 192.168.15.8:*; " +
           "script-src 'unsafe-inline' 'unsafe-eval' https: localhost:* 192.168.15.8:* 'self'; " +
           "connect-src https: localhost:* ws://localhost:* 192.168.15.8:* ws://192.168.15.8:*; " +
           "style-src 'unsafe-inline' https: localhost:* 192.168.15.8:* 'self'; " +
@@ -34,8 +35,8 @@ module.exports = (webpackConfigEnv, argv) => {
       hot: false,
       liveReload: false,
       port: 9000,
-      allowedHosts: 'all',
-      host: '0.0.0.0', // Isso permite acesso externo
+      allowedHosts: "all",
+      host: "0.0.0.0", // Isso permite acesso externo
     },
     plugins: [
       new HtmlWebpackPlugin({
